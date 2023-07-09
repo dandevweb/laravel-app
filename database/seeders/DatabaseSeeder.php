9227301648c4a506e8554482044933f35ea50543
Danilo Augusto <dandevweb@gmail.com>
2023-07-09 12:38:11 -0300

    chore: populate database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,13 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(100)->create();
 
+        $this->call([
+            CountrySeeder::class,
+            StateSeeder::class,
+            CitySeeder::class,
+            DepartmentSeeder::class,
+        ]);
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
